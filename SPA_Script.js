09372dd64c9fc7d4308d5aa65158f17a7498ed6c
renz <renz11082005@gmail.com>
2025-10-17 19:26:19 +0800

    Refactor SPA_Script.js for improved readability

--- SPA_Script.js
+++ SPA_Script.js
@@ -57,7 +57,7 @@ const talkCounter = setInterval(()=>{
 },1000);
 
 //FLOATING THINGS
-/*function floaters() {
+function floaters() {
     const heartSpin = document.createElement('div');
     heartSpin.className = 'heartSpin';
     heartSpin.style.left = Math.random() * 100 + 'vw';
@@ -73,6 +73,7 @@ const talkCounter = setInterval(()=>{
     setTimeout(() => heart.remove(), 5000);
     setTimeout(() => ribbon.remove(), 5000);
 }
-setInterval(floaters, 500);*/
+setInterval(floaters, 500);
+
 
 
